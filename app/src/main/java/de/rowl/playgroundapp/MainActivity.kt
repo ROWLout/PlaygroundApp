b39f206571f487a5f33f96357e1d7b830478b6a3
Roland Reteghi <r.reteghi@gmx.de>
2020-02-12 15:02:43 +0100

    introduced textView for DissplayMessageActivity

--- app/src/main/java/de/rowl/playgroundapp/MainActivity.kt
+++ app/src/main/java/de/rowl/playgroundapp/MainActivity.kt
@@ -3,20 +3,26 @@ package de.rowl.playgroundapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
-const val EXTRA_MESSAGE = "de.rowl.playgroundapp.MESSAGE"
+const val EXTRA_MESSAGE = "EXTRA_MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sendButton: Button = findViewById(R.id.send_button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        sendButton.setOnClickListener {
+            val message = editText.text.toString()
+            sendMessage(message)
+        }
     }
 
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
+    private fun sendMessage(message: String) {
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
