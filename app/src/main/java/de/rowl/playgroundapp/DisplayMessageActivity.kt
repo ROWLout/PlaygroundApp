52bd63667b0d0bae1a7ad413a91dfbf31b18ce2c
Roland Reteghi <r.reteghi@gmx.de>
2020-02-12 13:26:56 +0100

    created DsisplayMessageActivity

---
+++ app/src/main/java/de/rowl/playgroundapp/DisplayMessageActivity.kt
@@ -0,0 +1,12 @@
+package de.rowl.playgroundapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+    }
+}
