7e7d75f16e40ed1fe054df60324fefc26abca622
Roland Reteghi <r.reteghi@gmx.de>
2020-02-12 15:48:35 +0100

    added empty message validation

--- app/src/main/java/de/rowl/playgroundapp/MainActivity.kt
+++ app/src/main/java/de/rowl/playgroundapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 const val EXTRA_MESSAGE = "EXTRA_MESSAGE"
 
@@ -18,7 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         sendButton.setOnClickListener {
             val message = editText.text.toString()
-            sendMessage(message)
+            if(message.isNullOrEmpty()) {
+                Toast.makeText(applicationContext, "Bitte Text eingeben!", Toast.LENGTH_SHORT).show()
+            }else{
+                sendMessage(message)
+            }
         }
     }
 
